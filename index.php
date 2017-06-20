90bf66ccd508a42d61c7b4f4718955b663c2da08
myrac <mpavlino1@tvz.hr>
2017-06-20 21:10:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 	<meta name="author" content="Team Product">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" href="TVZ.png" type="image/x-icon"/>
 </head>
 
 <body>
